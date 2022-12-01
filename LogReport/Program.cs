379f40b34b054efe88e34842682672483d27b503
Asif Hasan <72686397+AsifHasanChowdhury@users.noreply.github.com>
2022-12-01 19:48:35 +0600

    update

--- LogReport/Program.cs
+++ LogReport/Program.cs
@@ -1,19 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
-string logFile = File.ReadAllText("C:\\Users\\shima\\Downloads\\logs\\20221019-All.log.0");
-string value = "2022-10-19 08:34:28.5931 [DEBUG] - [Thread:62  ] - [== == Inside UIServices.getMenuByPageName method. pageName:home|lid:74182761amandai]";
+string logFile = File.ReadAllText("C:\\Users\\SECL\\Downloads\\logs\\20221019-All.log.0");
+//string value = "2022-10-19 08:34:28.5931 [DEBUG] - [Thread:62  ] - [== == Inside UIServices.getMenuByPageName method. pageName:home|lid:74182761amandai]";
 //string pattern = @"{4}-{2}-{2}\s{2}:{2}:{2}.{4}\s[A-Z\s?]\s-\s[A-Z|a-z:{1,2}\s?";
+//string lidPattern=@" pageName:[A-z]*\|lid:\d{1,}";
+//List<Tuple<Match, List<string> >> TupleMatch = new List<Tuple<Match, List<string>>>();
+
+string lidPatternPage = @"pageName:[A-z]*\|lid:\d{1,}";
 
 string pattern = @"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{4}\s\[[A-Z]*\]\s-\s\[Thread:\d{1,3}\s*?\]\s?-\s?[==\s==\s^[A-z]*\s[A-z]*.[A-z]*\s[A-z]*.\s?[A-z]*:[A-z]*\|lid:\d{1,}[A-z]+\]";
-//string pattern = @" pageName:[A-z]*\|lid:\d{1,}";
+
+string lidPattern = @"lid:\d{1,}";
+
 MatchCollection matches = Regex.Matches(logFile, pattern);
-var count1 = 0;
-foreach (Match match in matches)
+
+MatchCollection LidPageMatchList = Regex.Matches(logFile, lidPatternPage);
+
+//List <String> idList=new List<String> ();
+
+IDictionary <String,List<Match>> Mapped=new Dictionary<String,List<Match>> ();
+
+foreach (Match match in LidPageMatchList)
 {
-    count1 += 1;
-    Console.WriteLine(match.Value);
+   Match id = Regex.Match(match.Value, lidPattern);
+
+    if (!Mapped.ContainsKey(id.Value))
+    {
+        Mapped.Add(id.Value.ToString(), new List<Match>());
+    }
 }
-Console.WriteLine(count1);
-//Console.WriteLine(logFile); 
+
+
+foreach (var match in Mapped)
+{
+    Console.WriteLine(match.Key);
+}
+
+
+
+//int count = 0;
+//foreach (Match match in LidPageMatchList)
+//{
+//    if (!UserTrackList.ContainsKey(match))
+//    {
+//        count += 1;
+//        UserTrackList.Add(match, new List<string>());
+//    }
+//}
+
+//foreach (var item in UserTrackList)
+//{
+//    Console.WriteLine(item.Key + " Key & Value " + item.Value);
+//}
+
+
+
+//UserTrackList.Add(match1, test);
+
+//foreach (var val in UserTrackList)
+//{
+//    Console.Write(val.Key);
+
+//    Console.Write("[ ");
+//    foreach (var val2 in val.Value)
+//    {
+//        Console.Write(" value pair "+ val2 +" ,");
+//    }
+//    Console.WriteLine(" ]");
+//}
+//Console.WriteLine(UserTrackList.Values);
+
