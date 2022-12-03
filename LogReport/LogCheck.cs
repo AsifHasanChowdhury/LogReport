2e1da7fc7dee7bee3ab458ad5e83ff5e8831e86c
Asif Hasan <106883390+AsifShimanto@users.noreply.github.com>
2022-12-04 03:00:49 +0600

    update

---
+++ LogReport/LogCheck.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogReport
+{
+    internal class LogCheck
+    {
+        //foreach (var val in Mapped)
+        //{
+        //    Console.Write(val.Key);
+
+        //    Console.Write("[ ");
+        //    foreach (var val2 in val.Value)
+        //    {
+        //        Console.Write(" value pair " + val2 + " ,");
+        //    }
+        //    Console.WriteLine(" ]");
+        //}
+
+
+        //int count = 0;
+        //foreach (Match match in LidPageMatchList)
+        //{
+        //    if (!UserTrackList.ContainsKey(match))
+        //    {
+        //        count += 1;
+        //        UserTrackList.Add(match, new List<string>());
+        //    }
+        //}
+
+        //foreach (var item in UserTrackList)
+        //{
+        //    Console.WriteLine(item.Key + " Key & Value " + item.Value);
+        //}
+
+
+
+        //UserTrackList.Add(match1, test);
+
+        //foreach (var val in UserTrackList)
+        //{
+        //    Console.Write(val.Key);
+
+        //    Console.Write("[ ");
+        //    foreach (var val2 in val.Value)
+        //    {
+        //        Console.Write(" value pair "+ val2 +" ,");
+        //    }
+        //    Console.WriteLine(" ]");
+        //}
+        //Console.WriteLine(UserTrackList.Values);
+
+        //string pattern = @"{4}-{2}-{2}\s{2}:{2}:{2}.{4}\s[A-Z\s?]\s-\s[A-Z|a-z:{1,2}\s?";
+        //string lidPattern=@" pageName:[A-z]*\|lid:\d{1,}";
+        //List<Tuple<Match, List<string> >> TupleMatch = new List<Tuple<Match, List<string>>>();
+        //List <String> idList=new List<String> ();
+        //MatchCollection LidPageMatchList = Regex.Matches(logFile, lidPatternPage);
+    }
+}
--- LogReport/Program.cs
+++ LogReport/Program.cs
@@ -5,80 +5,138 @@ using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using static System.Net.Mime.MediaTypeNames;
 
-string logFile = File.ReadAllText("C:\\Users\\SECL\\Downloads\\logs\\20221019-All.log.0");
-//string value = "2022-10-19 08:34:28.5931 [DEBUG] - [Thread:62  ] - [== == Inside UIServices.getMenuByPageName method. pageName:home|lid:74182761amandai]";
-//string pattern = @"{4}-{2}-{2}\s{2}:{2}:{2}.{4}\s[A-Z\s?]\s-\s[A-Z|a-z:{1,2}\s?";
-//string lidPattern=@" pageName:[A-z]*\|lid:\d{1,}";
-//List<Tuple<Match, List<string> >> TupleMatch = new List<Tuple<Match, List<string>>>();
-//List <String> idList=new List<String> ();
-
-
-string lidPatternPage = @"pageName:[A-z]*\|lid:\d{1,}";
-
-string pattern = @"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{4}\s\[[A-Z]*\]\s-\s\[Thread:\d{1,3}\s*?\]\s?-\s?[==\s==\s^[A-z]*\s[A-z]*.[A-z]*\s[A-z]*.\s?[A-z]*:[A-z]*\|lid:\d{1,}[A-z]+\]";
-
-string lidPattern = @"lid:\d{1,}";
-
-string FinalPage = @"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{4}\s\[[A-Z]*\]\s-\s\[Thread:\d{1,3}\s*?\]\s?-\s?[==\s==\s^[A-z]*\s[A-z]*.[A-z]*\s[A-z]*.\s?[A-z]*:[A-z]*\|";
-
-MatchCollection matches = Regex.Matches(logFile, pattern);
-
-MatchCollection LidPageMatchList = Regex.Matches(logFile, lidPatternPage);
-
-IDictionary <String,List<Match>> Mapped=new Dictionary<String,List<Match>> ();
-
-foreach (Match match in matches)
+public class Program
 {
-   Match id = Regex.Match(match.Value, lidPattern);
-   
-    if (!Mapped.ContainsKey(id.Value))
+
+
+    static string lidPatternPage = @"pageName:[A-z]*";
+
+    static string pattern = @"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{4}\s\[[A-Z]*\]\s-\s\[Thread:\d{1,3}\s*?\]\s?-\s?[==\s==\s^[A-z]*\s[A-z]*.[A-z]*\s[A-z]*.\s?[A-z]*:[A-z]*\|lid:\d{1,}[A-z]+\]";
+
+    static string lidPattern = @"lid:\d{1,}";
+
+    static string FinalPage = @"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{4}\s\[[A-Z]*\]\s-\s\[Thread:\d{1,3}\s*?\]\s?-\s?[==\s==\s^[A-z]*\s[A-z]*.[A-z]*\s[A-z]*.\s?[A-z]*:[A-z]*\|";
+
+    //MatchCollection matches = Regex.Matches(logFile, pattern);
+
+    static IDictionary<String, List<Match>> Mapped = new Dictionary<String, List<Match>>();
+
+    static IDictionary<String, List<Match>> MappedPage = new Dictionary<String, List<Match>>();
+
+
+
+    public static void ReadLog()
     {
-        Mapped.Add(id.Value.ToString(), new List<Match>());
+            var filePaths = Directory
+            .EnumerateFiles(@"C:\Users\shima\Downloads\logs\")
+            .Where(f => f.EndsWith("All.log.0"));
+        
+            foreach (var file in filePaths)
+            {
+                MatchCollection matches = Regex
+                .Matches(File.ReadAllText(file.ToString()), pattern);
+
+            FindUserTrace((Dictionary<string, List<Match>>)MappedPage,
+                          (Dictionary<String, List<Match>>)Mapped,
+                          lidPatternPage,
+                          FinalPage,
+                          lidPattern,
+                          matches);
+
+
+
+
+        }
+        //Console.WriteLine(filePaths);
+    }
+
+
+
+    public static void PrintDetail(Dictionary <String, List<Match>> MappedPage) {
+
+        foreach (var val in MappedPage)
+        {
+            Console.Write(val.Key);
+
+            Console.Write("[");
+            foreach (var val2 in val.Value)
+            {
+                Console.Write(" " + val2 + "=>");
+            }
+            Console.WriteLine("]");
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Total Number of Unique Users "+MappedPage.Keys.Count());
+
+    }
+
+
+
+    public static void FindUserTrace(
+        Dictionary<String, List<Match>> MappedPage,
+        Dictionary<String, List<Match>> Mapped,
+        string lidPatternPage, 
+        string FinalPage,
+        string lidPattern,
+        MatchCollection matches)
+
+    {
+        foreach (Match match in matches)
+        {
+            Match id = Regex.Match(match.Value, lidPattern);
+
+
+            if (!Mapped.ContainsKey(id.Value))
+            {
+                List<Match> pagelist = new List<Match>();
+
+                List<Match> page = new List<Match>();
+
+                Mapped.Add(id.Value.ToString(), pagelist);
+
+                MappedPage.Add(id.Value.ToString(), page);
+            }
+
+            if (Regex.IsMatch(match.Value, id.Value))
+            {
+                Match pageDetail = Regex.Match(match.Value, FinalPage);
+
+                Match page = Regex.Match(match.Value, lidPatternPage);
+
+
+                Mapped[id.Value.ToString()].Add(pageDetail);
+
+                MappedPage[id.Value.ToString()].Add(page);
+            }
+
+        }
+
+    }
+
+
+
+    public static void Main(String[] args)
+    {
+
+      //  string logFile = File.ReadAllText("C:\\Users\\shima\\Downloads\\logs\\20221019-All.log.0");
+
+
+
+
+        //FindUserTrace ((Dictionary<string, List<Match>>)MappedPage,
+        //              (Dictionary<String, List<Match>>)Mapped,
+        //              lidPatternPage,
+        //              FinalPage,
+        //              lidPattern,
+        //              matches);
+
+        //PrintDetail((Dictionary<string, List<Match>>)MappedPage);
+
+        ReadLog();
+        PrintDetail((Dictionary<string, List<Match>>)MappedPage);
+
+
     }
 }
-
-
-
-foreach (var match in Mapped)
-{
-
-   // foreach( var match2 in )
-
-    //if(Regex.IsMatch())
-    Console.WriteLine(match.Key);
-}
-
-
-
-//int count = 0;
-//foreach (Match match in LidPageMatchList)
-//{
-//    if (!UserTrackList.ContainsKey(match))
-//    {
-//        count += 1;
-//        UserTrackList.Add(match, new List<string>());
-//    }
-//}
-
-//foreach (var item in UserTrackList)
-//{
-//    Console.WriteLine(item.Key + " Key & Value " + item.Value);
-//}
-
-
-
-//UserTrackList.Add(match1, test);
-
-//foreach (var val in UserTrackList)
-//{
-//    Console.Write(val.Key);
-
-//    Console.Write("[ ");
-//    foreach (var val2 in val.Value)
-//    {
-//        Console.Write(" value pair "+ val2 +" ,");
-//    }
-//    Console.WriteLine(" ]");
-//}
-//Console.WriteLine(UserTrackList.Values);
-
